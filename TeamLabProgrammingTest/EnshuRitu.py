7fa2c11f191618c96ad5083a08fa3490dd0a413f
scarlet7 <shougo.ushiro@gmail.com>
2024-04-30 20:27:19 +0900

    fix

--- TeamLabProgrammingTest/EnshuRitu.py
+++
@@ -1,8 +0,0 @@
-A = list(map(int, (input())))
-count = [1000]
-
-for i in range(600):
-     count [A[i]*10 + A[i+1]] += 1
-        
-        
-print(count)
--- TeamLabProgrammingTest/Yakusu.py
+++
@@ -1,9 +0,0 @@
-n = 1022221200
-
-count = 0
-for i in range(10000, 100000):
-    if n % i == 0:
-        count += 1
-        
-        
-print(count)
